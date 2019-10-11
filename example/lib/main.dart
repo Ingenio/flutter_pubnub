cdb7f5ced41e01c49b4716e35248baccc1866e30
Olivier Brand <obrand@ingenio.com>
2019-10-11 16:10:36 -0600

    Added loggly

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,17 +14,17 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final _firstUserClient = PubNub(PubNubConfig(
-      'pub-c-9235bd41-31e7-405c-b1bd-8130e8138c88',
-      'sub-c-6de4a01a-e54a-11e9-9f1b-ce77373a3518',
+      'pub-c-9235bd41-31e7-405c-b1bd-8130e8138c88', 'sub-c-6de4a01a-e54a-11e9-9f1b-ce77373a3518',
       uuid: 'a0a80f2d-b48d-460c-b3bd-a244a877df1f'));
   final _secondUserClient = PubNub(PubNubConfig(
-      'pub-c-9235bd41-31e7-405c-b1bd-8130e8138c88',
-      'sub-c-6de4a01a-e54a-11e9-9f1b-ce77373a3518',
+      'pub-c-9235bd41-31e7-405c-b1bd-8130e8138c88', 'sub-c-6de4a01a-e54a-11e9-9f1b-ce77373a3518',
       presenceTimeout: 120,
       uuid: '127c1ab5-fc7f-4c46-8460-3207b6782007',
       filter: 'uuid != "127c1ab5-fc7f-4c46-8460-3207b6782007"'));
 
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  String _token;
 
   @override
   void initState() {
@@ -66,13 +66,20 @@ class _MyAppState extends State<MyApp> {
         print("onResume: $message");
       },
     );
+
+    Stream<String> fcmStream = _firebaseMessaging.onTokenRefresh;
+
+    fcmStream.listen((token) {
+      if (token != null) {
+        _token = token;
+        sendEvent('TOKEN REFRESHED', token);
+      }
+    });
   }
 
   void iOSPermission() {
-    _firebaseMessaging.requestNotificationPermissions(
-        IosNotificationSettings(sound: true, badge: true, alert: true));
-    _firebaseMessaging.onIosSettingsRegistered
-        .listen((IosNotificationSettings settings) {
+    _firebaseMessaging.requestNotificationPermissions(IosNotificationSettings(sound: true, badge: true, alert: true));
+    _firebaseMessaging.onIosSettingsRegistered.listen((IosNotificationSettings settings) {
       print("Settings registered: $settings");
     });
   }
@@ -80,8 +87,7 @@ class _MyAppState extends State<MyApp> {
   Future sendEvent(String eventName, String message) async {
     Dio dio = new Dio(); // with default Options
 
-    await dio.post(
-        "http://logs-01.loggly.com/inputs/844af57e-4059-430a-923d-1151c23af240/tag/http",
+    await dio.post("http://logs-01.loggly.com/inputs/336a5a72-0233-4403-9218-405f5516f266/tag/http",
         data: {"flutter_pubnub": eventName, "message": message});
   }
 
@@ -99,42 +105,35 @@ class _MyAppState extends State<MyApp> {
           appBar: AppBar(
             title: const Text('PubNub'),
           ),
-          body: Column(mainAxisAlignment: MainAxisAlignment.center, children: <
-              Widget>[
+          body: Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
             Padding(
                 padding: EdgeInsets.only(left: 8.0),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[Text('Pub/Sub/Publish')])),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  FlatButton(
-                      color: Colors.black12,
-                      onPressed: () {
-                        _firstUserClient.unsubscribe(['Channel']);
-                        _secondUserClient.unsubscribe(['Channel']);
-                      },
-                      child: Text('Unsubscribe')),
-                  FlatButton(
-                      color: Colors.black12,
-                      onPressed: () {
-                        _firstUserClient
-                            .subscribe(['Channel', 'Channel-pndebug']);
-                        _secondUserClient.subscribe(['Channel']);
-                      },
-                      child: Text('Subscribe')),
-                  FlatButton(
-                      color: Colors.black12,
-                      onPressed: () {
-                        _firstUserClient.publish(['Channel', 'Channel2'],
-                            {'message': 'Hello World!'});
-                        //_secondUserClient.publish(['Channel'], {'message': 'Hello First User!'},
-                        //   metadata: {'uuid': '127c1ab5-fc7f-4c46-8460-3207b6782007'});
-                        // _firstUserClient.presence(['Channel'], {'state': 'AFK'});
-                      },
-                      child: Text('Send Message')),
-                ]),
+                child: Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[Text('Pub/Sub/Publish')])),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: <Widget>[
+              FlatButton(
+                  color: Colors.black12,
+                  onPressed: () {
+                    _firstUserClient.unsubscribe(['Channel']);
+                    _secondUserClient.unsubscribe(['Channel']);
+                  },
+                  child: Text('Unsubscribe')),
+              FlatButton(
+                  color: Colors.black12,
+                  onPressed: () {
+                    _firstUserClient.subscribe(['Channel', 'Channel-pndebug']);
+                    _secondUserClient.subscribe(['Channel']);
+                  },
+                  child: Text('Subscribe')),
+              FlatButton(
+                  color: Colors.black12,
+                  onPressed: () {
+                    _firstUserClient.publish(['Channel', 'Channel2'], {'message': 'Hello World!'});
+                    //_secondUserClient.publish(['Channel'], {'message': 'Hello First User!'},
+                    //   metadata: {'uuid': '127c1ab5-fc7f-4c46-8460-3207b6782007'});
+                    // _firstUserClient.presence(['Channel'], {'state': 'AFK'});
+                  },
+                  child: Text('Send Message')),
+            ]),
             Padding(
                 padding: EdgeInsets.all(
                   8.0,
@@ -145,67 +144,53 @@ class _MyAppState extends State<MyApp> {
                 )),
             Padding(
                 padding: EdgeInsets.only(left: 8.0),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[Text('Channel Group')])),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  FlatButton(
-                      color: Colors.black12,
-                      onPressed: () {
-                        _firstUserClient.addChannelsToChannelGroup(
-                            'Group1', ['Channel', 'Channel2']);
-                      },
-                      child: Text('Add')),
-                  FlatButton(
-                      color: Colors.black12,
-                      onPressed: () {
-                        _firstUserClient
-                            .listChannelsForChannelGroup('Group1')
-                            .then((channels) {
-                          print("Channels in Group 1: $channels");
-                        });
-                      },
-                      child: Text('List')),
-                  FlatButton(
-                      color: Colors.black12,
-                      onPressed: () {
-                        _firstUserClient.removeChannelsFromChannelGroup(
-                            'Group1', ['Channel']).then((void arg) {
-                          _firstUserClient
-                              .listChannelsForChannelGroup('Group1')
-                              .then((channels) {
-                            print(
-                                "Channels in Group 1 after deletion: $channels");
-                          });
-                        });
-                      },
-                      child: Text('Remove')),
-                ]),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  FlatButton(
-                      color: Colors.black12,
-                      onPressed: () {
-                        _firstUserClient.deleteChannelGroup('Group1');
-                      },
-                      child: Text('Delete')),
-                  FlatButton(
-                      color: Colors.black12,
-                      onPressed: () {
-                        _firstUserClient.subscribeToChannelGroups(['Group1']);
-                      },
-                      child: Text('Subscribe')),
-                  FlatButton(
-                      color: Colors.black12,
-                      onPressed: () {
-                        _firstUserClient
-                            .unsubscribeFromChannelGroups(['Group1']);
-                      },
-                      child: Text('Unsubscribe')),
-                ]),
+                child: Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[Text('Channel Group')])),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: <Widget>[
+              FlatButton(
+                  color: Colors.black12,
+                  onPressed: () {
+                    _firstUserClient.addChannelsToChannelGroup('Group1', ['Channel', 'Channel2']);
+                  },
+                  child: Text('Add')),
+              FlatButton(
+                  color: Colors.black12,
+                  onPressed: () {
+                    _firstUserClient.listChannelsForChannelGroup('Group1').then((channels) {
+                      print("Channels in Group 1: $channels");
+                    });
+                  },
+                  child: Text('List')),
+              FlatButton(
+                  color: Colors.black12,
+                  onPressed: () {
+                    _firstUserClient.removeChannelsFromChannelGroup('Group1', ['Channel']).then((void arg) {
+                      _firstUserClient.listChannelsForChannelGroup('Group1').then((channels) {
+                        print("Channels in Group 1 after deletion: $channels");
+                      });
+                    });
+                  },
+                  child: Text('Remove')),
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: <Widget>[
+              FlatButton(
+                  color: Colors.black12,
+                  onPressed: () {
+                    _firstUserClient.deleteChannelGroup('Group1');
+                  },
+                  child: Text('Delete')),
+              FlatButton(
+                  color: Colors.black12,
+                  onPressed: () {
+                    _firstUserClient.subscribeToChannelGroups(['Group1']);
+                  },
+                  child: Text('Subscribe')),
+              FlatButton(
+                  color: Colors.black12,
+                  onPressed: () {
+                    _firstUserClient.unsubscribeFromChannelGroups(['Group1']);
+                  },
+                  child: Text('Unsubscribe')),
+            ]),
             Padding(
                 padding: EdgeInsets.all(
                   8.0,
@@ -216,25 +201,21 @@ class _MyAppState extends State<MyApp> {
                 )),
             Padding(
                 padding: EdgeInsets.only(left: 8.0),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[Text('History')])),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  FlatButton(
-                      color: Colors.black12,
-                      onPressed: () {
-                        _firstUserClient.history('Channel', 1).then((items) {
-                          if (items != null && items.isNotEmpty) {
-                            print("Last Item: $items");
-                          } else {
-                            print('No items');
-                          }
-                        });
-                      },
-                      child: Text('Last')),
-                ]),
+                child: Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[Text('History')])),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: <Widget>[
+              FlatButton(
+                  color: Colors.black12,
+                  onPressed: () {
+                    _firstUserClient.history('Channel', 1).then((items) {
+                      if (items != null && items.isNotEmpty) {
+                        print("Last Item: $items");
+                      } else {
+                        print('No items');
+                      }
+                    });
+                  },
+                  child: Text('Last')),
+            ]),
             Padding(
                 padding: EdgeInsets.all(
                   8.0,
@@ -245,59 +226,62 @@ class _MyAppState extends State<MyApp> {
                 )),
             Padding(
                 padding: EdgeInsets.only(left: 8.0),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[Text('Push Notifications')])),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  FlatButton(
-                      color: Colors.black12,
-                      onPressed: () {
-                        _firebaseMessaging.getToken().then((token) {
-                          print("Token: $token");
-                          _firstUserClient.addPushNotificationsOnChannels(
-                              PushType.FCM, token, ['Channel']);
-                        });
-                      },
-                      child: Text('Add')),
-                  FlatButton(
-                      color: Colors.black12,
-                      onPressed: () {
-                        _firebaseMessaging.getToken().then((token) {
-                          print("Token: $token");
-                          _firstUserClient
-                              .listPushNotificationChannels(PushType.FCM, token)
-                              .then((channels) {
-                            print("Push Notes Channels: $channels");
-                            sendEvent(
-                                'PUSH CHANNELS', channels.toList().toString());
-                          });
-                        });
-                      },
-                      child: Text('List')),
-                  FlatButton(
-                      color: Colors.black12,
-                      onPressed: () {
-                        _firebaseMessaging.getToken().then((token) {
-                          print("Token: $token");
-                          _firstUserClient.removePushNotificationsFromChannels(
-                              PushType.FCM, token, ['Channel']);
-                        });
-                      },
-                      child: Text('Remove')),
-                  FlatButton(
-                      color: Colors.black12,
-                      onPressed: () {
-                        _firebaseMessaging.getToken().then((token) {
-                          print("Token: $token");
-                          _firstUserClient
-                              .removeAllPushNotificationsFromDeviceWithPushToken(
-                                  PushType.FCM, token);
-                        });
-                      },
-                      child: Text('Remove All')),
-                ]),
+                child: Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[Text('Push Notifications')])),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: <Widget>[
+              FlatButton(
+                  color: Colors.black12,
+                  onPressed: () {
+                    _firebaseMessaging.getToken().then((token) {
+                      print("Token: $token");
+                      _firstUserClient.addPushNotificationsOnChannels(PushType.FCM, token, ['Channel']);
+                      sendEvent('ADD CHANNELS', ['Channel'].toList().toString());
+                    });
+                  },
+                  child: Text('Add')),
+              FlatButton(
+                  color: Colors.black12,
+                  onPressed: () {
+                    _firebaseMessaging.getToken().then((token) {
+                      print("Token: $token");
+                      _firstUserClient.listPushNotificationChannels(PushType.FCM, token).then((channels) {
+                        print("Push Notes Channels: $channels");
+                        sendEvent('LIST CHANNELS', channels.toList().toString());
+                      });
+                    });
+                  },
+                  child: Text('List')),
+              FlatButton(
+                  color: Colors.black12,
+                  onPressed: () {
+                    _firebaseMessaging.getToken().then((token) {
+                      print("Token: $token");
+                      _firstUserClient.removePushNotificationsFromChannels(PushType.FCM, token, ['Channel']);
+                      sendEvent('REMOVE CHANNELS', ['Channel'].toList().toString());
+                    });
+                  },
+                  child: Text('Remove')),
+              FlatButton(
+                  color: Colors.black12,
+                  onPressed: () {
+                    _firebaseMessaging.getToken().then((token) {
+                      print("Token: $token");
+                      _firstUserClient.removeAllPushNotificationsFromDeviceWithPushToken(PushType.FCM, token);
+                      sendEvent('REMOVE ALL CHANNELS', token);
+                    });
+                  },
+                  child: Text('Remove All')),
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: <Widget>[
+              FlatButton(
+                  color: Colors.black12,
+                  onPressed: () {
+                    _firebaseMessaging.getToken().then((token) {
+                      print("Token: $token");
+                      sendEvent('TOKEN', token);
+                    });
+                  },
+                  child: Text('Token')),
+            ])
           ]),
         ),
       );
